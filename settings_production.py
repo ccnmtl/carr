0c24e5e9fb4e9fa00366579ee18a1d3a4efa4899
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2010-02-15 10:25:06 -0500

    initial import of carr source tree

---
+++ settings_production.py
@@ -0,0 +1,10 @@
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/carr/carr/templates",
+)
+
+MEDIA_ROOT = '/var/www/carr/uploads/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
---
+++ x/run_eddie.sh
@@ -0,0 +1,4 @@
+cd /home/eddie/sandbox/worth/
+echo 'STARTING DEV SERVER on http://192.168.122.1:8002'
+/home/eddie/sandbox/worth/manage.py runserver 192.168.122.1:8002
+
