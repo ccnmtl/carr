f68adb37510afaa6ed960764fe87a794ad63fa4c
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2010-04-26 10:38:30 -0400

    working save and reload on bruise_recon

--- activity_bruise_recon/media/js/bruise_recon.js
+++ activity_bruise_recon/media/js/bruise_recon.js
@@ -13,8 +13,6 @@ function maybeEnableNext()
     gonext = true;
    }
    
-   
-   
    if (gonext){
          setStyle('next', {'display': 'inline'}) 
    }
@@ -24,105 +22,42 @@ function maybeEnableNext()
   }
 }
 
-
-
-/*
-function connectCallouts()
-{
-   if (getElement("dosage_2"))
-   {
-      connectCalloutsDouble()
-   }
-   else
-   {  
-      connectCalloutsSingle()
-   }
-}
-
-function connectCalloutsDouble()
-{
-   vertical_line(getElement('dosage_callout'), getElement('dosage'))
-   vertical_line(getElement('disp_callout'), getElement('disp'))
-   vertical_line(getElement('refills_callout'), getElement('refills'))
-}
-
-function connectCalloutsSingle()
-{
-   vertical_line(getElement('dosage_callout'), getElement('dosage'))
-   vertical_line(getElement('disp_callout'), getElement('disp'))
-   vertical_line(getElement('refills_callout'), getElement('refills'))
-   
-}
-
-function horizontal_line(topElement, bottomElement)
-{
-   bottomPos = getElementPosition(bottomElement)
-   bottomDim = getElementDimensions(bottomElement)
-   
-   topPos = getElementPosition(topElement)
-   topDim = getElementDimensions(topElement)
-   
-   fromx = topPos.x + topDim.w/2
-   fromy = topPos.y + topDim.h
-   tox = bottomPos.x + bottomDim.w/2
-   toy = bottomPos.y
-   drawlines(fromx, fromy, tox, toy, fromx)
-
-}
-
-function vertical_line(leftElement, rightElement)
-{
-   x = getElementPosition(leftElement).x + getElementDimensions(leftElement).w
-   x2 = getElementPosition(rightElement).x
-   y = getElementPosition(rightElement).y + getElementDimensions(rightElement).h/2
-   
-   drawlines(x, y, x2, y, x2)
-}
-
-function drawlines (from_x, from_y, to_x, to_y, x_break) {
-    // make the left hline go all the way to the right edge of the center vline. Add 2 pixels, if necessary.
-    var extra = ( from_y > to_y )? 2 : 0; 
-    hline (from_x, x_break + extra, from_y);
-    vline (from_y, to_y, x_break);
-    hline (x_break, to_x, to_y );
-}
-
-function hline (from, to, y) {
-    if (from > to) { var temp = to; to = from; from = temp; }
-    var newdiv = DIV ( {"class":"connecting_line" });
-    appendChildNodes(currentDocument().body, newdiv);
-    setStyle( newdiv , { "left": from + 'px', "top" : y + 'px' , "width" : (to - from) + "px", "height" : "2px"});
-}
-
-function vline (from, to, x) {
-    if (from > to) { var temp = to; to = from; from = temp; }
-    var newdiv = DIV ( {"class":"connecting_line" });
-    appendChildNodes(currentDocument().body, newdiv);
-    setStyle( newdiv , { "left": x + 'px', "top" : from + 'px' , "height" : (to - from) + "px", "width" : "2px"});
-}
-
-function setBackgroundColor(ctrl)
-{
-   if (ctrl.value.length > 0)
-   {
-      setStyle(ctrl.id, { 'background-color': 'white' })
-   }
-   else
-   {
-      setStyle(ctrl.id, { 'background-color': '#f8db9f' })
-   }
-}
-
-function onEditChange(ctrl)
-{
-   setBackgroundColor(ctrl)
-   maybeEnableNext()
-}
-
-
 function loadStateSuccess(doc)
 {
    debug('loadStateSuccess')
+  case_name =  $('case_name').innerHTML;
+   
+   if (doc && doc[case_name])
+   {
+    state_for_this_page = doc[case_name];
+    logDebug (serializeJSON(state_for_this_page));
+   }
+   if (state_for_this_page['answered']) {
+        if (state_for_this_page['answered'] == 'yes') {
+            addElementClass($('answer_yes'), 'button_selected');
+        }
+        else if (state_for_this_page['answered'] == 'no') {
+            addElementClass($('answer_no'), 'button_selected');
+        }
+    }
+   if (state_for_this_page['factors']) {
+        factors_as_string = state_for_this_page['factors'].join (',');
+        logDebug (factors_as_string); 
+   }
+   if (factors_as_string.match(/patterns/) != null) {
+            addElementClass($('patterns'), 'button_selected');
+   }
+   if (factors_as_string.match(/severity/) != null) {
+            addElementClass($('severity'), 'button_selected');
+   }
+   if (factors_as_string.match(/location/) != null) {
+            addElementClass($('location'), 'button_selected');
+   }
+   if (factors_as_string.match(/explanation/) != null) {
+            addElementClass($('explanation'), 'button_selected');
+   }
+   
+   /*
    
    if (doc && doc[$('medication_name').value])
    {
@@ -159,7 +94,7 @@ function loadStateSuccess(doc)
    
   if ($('dosage_correct'))
      connectCallouts()
-     
+   */ 
   maybeEnableNext()
 }
 
@@ -169,46 +104,17 @@ function loadStateError(err)
    // @todo: Find a spot to display an error or decide just to fail gracefully
    // $('errorMsg').innerHTML = "An error occurred loading your state (" + err + "). Please start again."
 }
-*/
 
-//aasd = partial (like_checkbox, 'button_selected', 'bruise_recon_checkbox_div', 'patterns'      )
-  
-  
-  /* if (!$('dosage_correct'))
-   {
-      setStyle('next', {'display': 'none'})
-   }
-   url = 'http://' + location.hostname + ':' + location.port + "/activity/prescription/load/"
-   deferred = loadJSONDoc(url)
-   deferred.addCallbacks(loadStateSuccess, loadStateError)
 
-}
-*/
-
-/*
-.button_selected {
-   background-color: #efc48d;
-}
-
-<div id="answer_yes" class="answer_button">Yes</div>
-*/
-
-  
-  
-  /*
-  
-  connect ('patterns',       'onclick', partial (like_checkbox, 'button_selected', 'bruise_recon_checkbox_div', 'patterns'      ))
-  connect ('severity',       'onclick', partial (like_checkbox, 'button_selected', 'bruise_recon_checkbox_div', 'severity'      ))
-  connect ('body_location',  'onclick', partial (like_checkbox, 'button_selected', 'bruise_recon_checkbox_div', 'body_location' ))
-  connect ('credibility',    'onclick', partial (like_checkbox, 'button_selected', 'bruise_recon_checkbox_div', 'credibility'   ))
-  */
-  
-  //connect ('patterns',  'onclick', partial (toggleElementClass, 'patterns',  'button_selected'));
-  
 
 function loadState()
 {
    debug("loadState")
+   
+   url = 'http://' + location.hostname + ':' + location.port + "/activity/bruise_recon/load/"
+   deferred = loadJSONDoc(url)
+   deferred.addCallbacks(loadStateSuccess, loadStateError)
+   
    hide_answer();
    
    
@@ -216,9 +122,9 @@ function loadState()
   connect ('answer_no',  'onclick', partial (like_checkbox, 'button_selected', 'answer_button', 'answer_no'))
   
   
-  connect ('patterns',      'onclick', partial (toggleElementClass,'button_selected',       'patterns' ));
-  connect ('severity',      'onclick', partial (toggleElementClass,'button_selected',       'severity' ));
-  connect ('location', 'onclick', partial (toggleElementClass,'button_selected',  'location' ));
+  connect ('patterns',      'onclick', partial (toggleElementClass,'button_selected',    'patterns' ));
+  connect ('severity',      'onclick', partial (toggleElementClass,'button_selected',    'severity' ));
+  connect ('location',      'onclick', partial (toggleElementClass,'button_selected',    'location' ));
   connect ('explanation',   'onclick', partial (toggleElementClass,'button_selected',    'explanation' ));
 
   connect('submit_div', 'onclick', show_answer);
@@ -263,11 +169,11 @@ function validate() {
 
 function answer_is_correct() {
    
-    answer_is_yes = ($('correct_answer').innerHTML.toLowerCase().match(/yes/).length > 0)
-    factors_include_severity = ($('correct_factors').innerHTML.toLowerCase().match(/severity/).length > 0)
-    factors_include_location = ($('correct_factors').innerHTML.toLowerCase().match(/location/).length > 0)
-    factors_include_patterns = ($('correct_factors').innerHTML.toLowerCase().match(/patterns/).length > 0)
-    factors_include_explanation = ($('correct_factors').innerHTML.toLowerCase().match(/explanation/).length > 0)
+    answer_is_yes = ($('correct_answer').innerHTML.toLowerCase().match(/yes/))
+    factors_include_severity = ($('correct_factors').innerHTML.toLowerCase().match(/severity/))
+    factors_include_location = ($('correct_factors').innerHTML.toLowerCase().match(/location/))
+    factors_include_patterns = ($('correct_factors').innerHTML.toLowerCase().match(/patterns/))
+    factors_include_explanation = ($('correct_factors').innerHTML.toLowerCase().match(/explanation/))
     
     if (answer_is_yes) {
         if (hasElementClass($('answer_no'), 'button_selected')) { return false };
@@ -297,11 +203,11 @@ function answer_is_correct() {
 }
 
 function show_answer() {
-    hideElement ('submit_div');
     if (!validate()) {
         alert ("Please choose yes or no.");
         return;
     }
+    hideElement ('submit_div');
     if (answer_is_correct()) {
         $("your_answer_was").innerHTML = "Your answer is correct.";
     }
@@ -320,36 +226,39 @@ MochiKit.Signal.connect(window, "onload", loadState)
 
 function saveState()
 {
-/*
-   if (!$('dosage_correct'))
+  case_name =  $('case_name').innerHTML;
+  
+  debug("saveState")
+  url = 'http://' + location.hostname + ':' + location.port + "/activity/bruise_recon/save/"
+
+   debug("saveState");
+   doc = {}
+   
+   if (hasElementClass($('answer_yes'), 'button_selected'))
    {
-      debug("saveState")
-      url = 'http://' + location.hostname + ':' + location.port + "/activity/prescription/save/"
-    
-      rx = {} 
-      rx['dosage'] = $('dosage').value
-      rx['disp'] = $('disp').value
-      rx['sig'] = $('sig').value
-      rx['refills'] = $('refills').value
-      
-      if ($('dosage_2'))
-      {
-         rx['dosage_2'] = $('dosage_2').value
-         rx['disp_2'] = $('disp_2').value
-         rx['sig_2'] = $('sig_2').value
-         rx['refills_2'] = $('refills_2').value
-      }
-      
-      doc = {}
-      doc[$('medication_name').value] = rx
-      
-      // save state via a synchronous request. 
-      var sync_req = new XMLHttpRequest();  
-      sync_req.onreadystatechange= function() { if (sync_req.readyState!=4) return false; }         
-      sync_req.open("POST", url, false);
-      sync_req.send(queryString({'json':JSON.stringify(doc, null)}));
+        doc ['answered'] = 'yes';
+   } else {
+        doc ['answered'] = 'no';
    }
-   */
+   
+   doc ['factors'] = []
+   if (hasElementClass($('severity'),    'button_selected')) { doc ['factors'].push (   'severity') };
+   if (hasElementClass($('location'),    'button_selected')) { doc ['factors'].push (   'location') };
+   if (hasElementClass($('patterns'),    'button_selected')) { doc ['factors'].push (   'patterns') };
+   if (hasElementClass($('explanation'), 'button_selected')) { doc ['factors'].push ('explanation') };
+   
+   
+   
+  var sync_req = new XMLHttpRequest();  
+  sync_req.onreadystatechange= function() { if (sync_req.readyState!=4) return false; }         
+  sync_req.open("POST", url, false);
+  
+  what_to_send = {}
+  what_to_send [case_name ] = doc
+  
+  
+  sync_req.send(queryString({'json':JSON.stringify(what_to_send , null)}));
+   
 }
 
 MochiKit.Signal.connect(window, "onbeforeunload", saveState)
