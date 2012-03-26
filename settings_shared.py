f194cd00ad50dd7f9511f37b4a44987c1ad7ca58
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-03-26 12:22:38 -0400

    edit to tests pursuant to maria content change

--- settings_shared.py
+++ settings_shared.py
@@ -8,6 +8,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'carr' # Or path to database file if using sqlite3.
 DATABASE_USER = 'pusher'             # Not used with sqlite3.
+DATABASE_USER = 'eddie'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
