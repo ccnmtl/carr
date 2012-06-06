339193b7e19917009d9cc6cd60fb3654abe3083f
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-06-06 12:36:55 -0400

    basic changes to tiur dev admin

--- x/run_dev_in_gnome_terminal.sh
+++ x/run_dev_in_gnome_terminal.sh
@@ -1,4 +1,4 @@
 cd /home/eddie/sandbox/carr
-gnome-terminal -e /home/eddie/sandbox/carr/x/run_socialwork.sh --hide-menubar --geometry=120x20+5+1100 -t '***CARE***'
+gnome-terminal -e /home/eddie/sandbox/carr/x/run_tiur_socialwork.sh --hide-menubar --geometry=120x20+5+1100 -t '***CARE***'
 
 
