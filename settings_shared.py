019bb974647d3b9e49ea41c142c6de551c7c32e1
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2012-06-18 16:46:21 -0400

    resetting settings_shared.py

--- settings_shared.py
+++ settings_shared.py
@@ -5,20 +5,13 @@ import re
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-
-DATABASE_USER = 'ccnmtl'                                             
-DATABASE_PASSWORD = 'one6Teen' 
-DATABASE_HOST = 'psqldev.cul.columbia.edu'  
-DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'carr' # Or path to database file if using sqlite3.
-
-#DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_NAME = 'carr' # Or path to database file if using sqlite3.
-#DATABASE_USER = 'pusher'             # Not used with sqlite3.
-#DATABASE_USER = 'eddie'             # Not used with sqlite3.
-#DATABASE_PASSWORD = ''         # Not used with sqlite3.
-#DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-#DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_USER = 'pusher'             # Not used with sqlite3.
+DATABASE_USER = 'eddie'             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 CACHE_BACKEND = 'locmem://'
 
