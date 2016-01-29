8b60ab42f8ccac0346551b4aea395f1361e55528
Nik Nyby <nnyby@columbia.edu>
2016-01-29 10:06:53 -0500

    add SiteIdMiddleware

---
+++ carr/middleware.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.contrib.sites.requests import RequestSite
+
+
+class SiteIdMiddleware(object):
+    def process_request(self, request):
+        if 'ssw' in RequestSite(request).domain:
+            settings.SITE_ID = 2
+        else:
+            settings.SITE_ID = 1
