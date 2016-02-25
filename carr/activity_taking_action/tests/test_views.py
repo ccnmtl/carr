454d73e50597d36a670f8681bf03ed0e669fa427
Anders Pearson <anders@columbia.edu>
2016-02-25 14:54:32 +0000

    simple view test coverage for activity_taking_action

---
+++ carr/activity_taking_action/tests/test_views.py
@@ -0,0 +1,40 @@
+from django.test import TestCase, RequestFactory
+from carr.activity_taking_action.views import (
+    loadstate, savestate, student)
+from .factories import UserFactory
+
+
+class LoadStateTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_get(self):
+        u = UserFactory()
+        r = self.factory.get("/loadstate")
+        r.user = u
+        response = loadstate(r)
+        self.assertEqual(response.status_code, 200)
+
+
+class SaveStateTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_post(self):
+        u = UserFactory()
+        r = self.factory.post("/savestate", dict(json='{}'))
+        r.user = u
+        response = savestate(r)
+        self.assertEqual(response.status_code, 200)
+
+
+class StudentTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_get(self):
+        u = UserFactory()
+        r = self.factory.get("/student")
+        r.user = u
+        response = student(r, u.id)
+        self.assertEqual(response.status_code, 200)
