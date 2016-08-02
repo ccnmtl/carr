d3392c74eed61ea8bbd32d8fe00d9d0a5f200303
Anders Pearson <anders@columbia.edu>
2016-08-02 15:51:26 +0100

    route to imported callables instead of strings
    deprecated in Django 1.9, removed in 1.10

--- carr/activity_taking_action/urls.py
+++ carr/activity_taking_action/urls.py
@@ -1,11 +1,13 @@
+import os.path
+import django.views.static
+
 from django.conf.urls import url
 from .views import LoadStateView, SaveStateView, StudentView
-import os.path
 
 media_root = os.path.join(os.path.dirname(__file__), "media")
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media/(?P<path>.*)$', django.views.static.serve,
         {'document_root': media_root}),
     url(r'^load/$', LoadStateView.as_view()),
     url(r'^save/$', SaveStateView.as_view()),
