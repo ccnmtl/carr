9878759f165228ea7bde08786d273503d21c6c26
Susan Dreher <sdreher@gmail.com>
2021-09-30 19:55:39 -0400

    Fix KeyError for variable reference

--- carr/settings_shared.py
+++ carr/settings_shared.py
@@ -122,5 +122,6 @@ LTI_TOOL_CONFIGURATION = {
     'navigation': False,
     'new_tab': True,
     'frame_width': 1024,
-    'frame_height': 1024
+    'frame_height': 1024,
+    'allow_ta_access': False
 }
