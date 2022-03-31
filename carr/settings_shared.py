32d20147f1064081b40914c4cdc8726c649424b9
Susan Dreher <sdreher@gmail.com>
2022-03-31 13:01:09 -0400

    Remove admins from settings

--- carr/settings_shared.py
+++ carr/settings_shared.py
@@ -78,11 +78,6 @@ NON_ANONYMOUS_PATHS = COURSEAFFILS_PATHS
 COURSEAFFILS_EXEMPT_PATHS = ANONYMOUS_PATHS
 COURSEAFFIL_AUTO_MAP_GROUPS = ['demo']
 
-DEFAULT_SOCIALWORK_FACULTY_UNIS = [
-    'amo1', 'dls3', 'vc2162', 'jkt4', 'hh2537', 'mj52',
-    'ek2864', 'mr4179', 'hr2412'
-]
-
 # Pageblocks/Pagetree settings
 PAGEBLOCKS = ['pageblocks.HTMLBlock',
               'pageblocks.PullQuoteBlock',
